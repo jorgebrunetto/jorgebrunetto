319ed32383ebad724e0288db0652a00e8e7d917e
Jorge Brunetto <jorge.brunetto@loggi.com>
2025-01-14 16:38:20 -0300

    feat: icons SI

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -3,10 +3,14 @@ import { type IconBaseProps } from "react-icons/lib";
 
 import * as GiIcon from "react-icons/gi";
 import * as PiIcon from "react-icons/pi";
+import * as SiIcon from "react-icons/si";
 
 import { match, P } from "ts-pattern";
 
-export type IconName = keyof typeof PiIcon | keyof typeof GiIcon;
+export type IconName =
+  | keyof typeof PiIcon
+  | keyof typeof GiIcon
+  | keyof typeof SiIcon;
 
 export type IconProps = IconBaseProps & {
   name: IconName;
@@ -16,6 +20,7 @@ export function Icon({ name, size = 24, ...props }: IconProps) {
   const element = match(name)
     .with(P.string.startsWith("Pi"), icon => PiIcon[icon])
     .with(P.string.startsWith("Gi"), icon => GiIcon[icon])
+    .with(P.string.startsWith("Si"), icon => SiIcon[icon])
     .otherwise(() => React.Fragment);
 
   return React.createElement(element, { size, ...props });
