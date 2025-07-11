71c6bb07aa111e948d2fcff12ae7dd5d45a72834
Jorge Brunetto <jorge.brunetto@loggi.com>
2025-07-11 10:48:17 -0300

    feat: subida site com metas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,17 +32,34 @@ export const metadata: Metadata = {
     siteName: "jorgebrunetto",
     locale: "pt_BR",
     type: "website",
+    countryName: "Brasil",
+    images: [
+      {
+        url: "https://jorgebrunetto.com.br/images/share-jorgebrunetto.jpg",
+        width: 200,
+        height: 200,
+        alt: "Jorge Brunetto - criação e desenvolvimento",
+      },
+    ],
+    phoneNumbers: "+55 15 98137-6495",
+    emails: "jorgebrunetto@hotmail.com",
   },
   twitter: {
+    description:
+      "Site profissional de Jorge Brunetto. Freelancer Frontend, com VueJS, React/Native, Angular, criação de sites, designer ux/ui, mail marketing, SEO para seus sites e sistemas em geral.",
     title: "Jorge Brunetto - criação e desenvolvimento",
     creator: "@jorgebrunetto",
     site: "https://jorgebrunetto.com.br",
     card: "summary_large_image",
+    images: "https://jorgebrunetto.com.br/images/share-jorgebrunetto.jpg",
   },
   robots: "index, follow",
   authors: [{ name: "Jorge Brunetto", url: "https://jorgebrunetto.com.br" }],
   appleWebApp: true,
   applicationName: "Jorge Brunetto",
+  publisher: "Jorge Brunetto - Vercel",
+  creator: "Jorge Brunetto",
+  generator: "Next.js",
 };
 
 export default function RootLayout({
