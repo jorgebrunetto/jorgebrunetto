4897aa070624f5f146f41529daa98bc2ca69d771
Jorge Brunetto <jorge.brunetto@loggi.com>
2025-07-11 11:10:38 -0300

    feat: subida de imagem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     countryName: "Brasil",
     images: [
       {
-        url: "https://jorgebrunetto.com.br/images/share-jorgebrunetto.jpg",
+        url: "https://jorgebrunetto.com.br/share-jorgebrunetto.jpg",
         width: 200,
         height: 200,
         alt: "Jorge Brunetto - criação e desenvolvimento",
@@ -51,7 +51,7 @@ export const metadata: Metadata = {
     creator: "@jorgebrunetto",
     site: "https://jorgebrunetto.com.br",
     card: "summary_large_image",
-    images: "https://jorgebrunetto.com.br/images/share-jorgebrunetto.jpg",
+    images: "https://jorgebrunetto.com.br/share-jorgebrunetto.jpg",
   },
   robots: "index, follow",
   authors: [{ name: "Jorge Brunetto", url: "https://jorgebrunetto.com.br" }],
@@ -69,6 +69,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <title>Jorge Brunetto - criação e desenvolvimento</title>
+        <meta
+          name="description"
+          content="Site profissional de Jorge Brunetto. Freelancer Frontend, com VueJS, React/Native, Angular, criação de sites, designer ux/ui, mail marketing, SEO para seus sites e sistemas em geral."
+        />
+
+        <meta property="og:url" content="https://jorgebrunetto.com.br" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Jorge Brunetto - criação e desenvolvimento"
+        />
+        <meta
+          property="og:description"
+          content="Site profissional de Jorge Brunetto. Freelancer Frontend, com VueJS, React/Native, Angular, criação de sites, designer ux/ui, mail marketing, SEO para seus sites e sistemas em geral."
+        />
+        <meta
+          property="og:image"
+          content="https://jorgebrunetto.com.br/share-jorgebrunetto.jpg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="jorgebrunetto.com.br" />
+        <meta property="twitter:url" content="https://jorgebrunetto.com.br" />
+        <meta
+          name="twitter:title"
+          content="Jorge Brunetto - criação e desenvolvimento"
+        />
+        <meta
+          name="twitter:description"
+          content="Site profissional de Jorge Brunetto. Freelancer Frontend, com VueJS, React/Native, Angular, criação de sites, designer ux/ui, mail marketing, SEO para seus sites e sistemas em geral."
+        />
+        <meta
+          name="twitter:image"
+          content="https://jorgebrunetto.com.br/share-jorgebrunetto.jpg"
+        />
+      </head>
+
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
